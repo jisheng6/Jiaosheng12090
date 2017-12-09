d73c97ef7531a8d1fab81663d6bbdd957b49cc53
jisheng6 <1278661505@QQ>
2017-12-09 15:23:43 +0800

    Initial commit

---
+++ app/src/main/java/com/example/adminjs/jiaosheng12090/CardDataItem.java
@@ -0,0 +1,13 @@
+package com.example.adminjs.jiaosheng12090;
+
+/**
+ * 卡片数据装载对象
+ *
+ * @author xmuSistone
+ */
+public class CardDataItem {
+    String imagePath;
+    String userName;
+    int likeNum;
+    int imageNum;
+}
